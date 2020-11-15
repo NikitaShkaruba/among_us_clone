62328d5c0f2c191b0ded62742c817df639a08bf9
Nikita Shkaruba <sh.sigmaone@gmail.com>
2020-11-15 13:57:17 +0300

    Add on demand logger event skipping from wanted sections

--- AmongUsCloneServer/src/AmongUsClone/Server/Infrastructure/Logger.cs
+++ AmongUsCloneServer/src/AmongUsClone/Server/Infrastructure/Logger.cs
@@ -1,22 +1,40 @@
 using System;
+using System.Linq;
 
 namespace AmongUsClone.Server.Infrastructure
 {
     public static class Logger
     {
+        private const string LogTypeEvent = "event";
+        private const string LogTypeError = "error";
+
         public static void LogEvent(LoggerSection loggerSection, string eventDescription)
         {
-            Log("event", loggerSection, eventDescription);
+            Log(LogTypeEvent, loggerSection, eventDescription);
         }
 
         public static void LogError(LoggerSection loggerSection, string errorDescription)
         {
-            Log("error", loggerSection, errorDescription);
+            Log(LogTypeError, loggerSection, errorDescription);
         }
 
         private static void Log(string logType, LoggerSection loggerSection, string logDescription)
         {
+            if (IsSkippedLoggerSection(loggerSection) && logType != LogTypeError)
+            {
+                return;
+            }
+            
             Console.WriteLine($"[{loggerSection} {logType}] {logDescription}");
+        }
+
+        private static bool IsSkippedLoggerSection(LoggerSection loggerSection)
+        {
+            LoggerSection[] skippedLoggerSections = {
+                LoggerSection.Network,
+            };
+
+            return skippedLoggerSections.Contains(loggerSection);
         }
     }
 }
