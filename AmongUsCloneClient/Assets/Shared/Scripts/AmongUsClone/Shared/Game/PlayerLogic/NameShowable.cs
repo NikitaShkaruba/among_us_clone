4691e9081ea1881c9ddb37a831746eaf4a131488
Nikita Shkaruba <sh.sigmaone@gmail.com>
2021-01-07 18:33:57 +0300

    Add player name tag to Lobby.prefab

---
+++ AmongUsCloneClient/Assets/Shared/Scripts/AmongUsClone/Shared/Game/PlayerLogic/NameShowable.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AmongUsClone.Shared.Game.PlayerLogic
+{
+    public class NameShowable : MonoBehaviour
+    {
+        public Text textLabel;
+        public PlayerInformation playerInformation;
+
+        public void Start()
+        {
+            textLabel.text = playerInformation.name;
+        }
+    }
+}
